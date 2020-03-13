ac00e726240ba4b7387d1239d637918d0e4852f8
adimx <1424769309@qq.com>
2020-03-14 00:18:22 +0800

    新增VIewBinding,免去findById的繁琐操作。完成用户下拉拖动布局再手动拖动复原不继续滚动scrollView效果。

--- app/src/main/java/com/ikudot/wechatuistudy/FriendCircleActivity.java
+++ app/src/main/java/com/ikudot/wechatuistudy/FriendCircleActivity.java
@@ -4,36 +4,23 @@ import android.annotation.TargetApi;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.DisplayCutout;
+import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RelativeLayout;
-import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.widget.NestedScrollView;
 
 
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners;
 import com.bumptech.glide.request.RequestOptions;
 import com.ikudot.wechatuistudy.Util.Utils;
+import com.ikudot.wechatuistudy.databinding.FriendCircleBinding;
 import com.ikudot.wechatuistudy.listener.FriendCircleScrollViewListener;
 
 public class FriendCircleActivity extends AppCompatActivity implements View.OnTouchListener {
     private static final String TAG = "FriendCircleActivity";
-    ImageView userHead; //用户头像
-    RelativeLayout topBar; //悬浮标题栏布局
-    RelativeLayout backButton; //返回按钮
-    ImageView backIcon;//返回按钮图标
-    TextView title;//标题文字
-    RelativeLayout photoLayout;//发朋友圈按钮
-    ColorIconView photoIcon;//发朋友圈按钮图标
-    NestedScrollView scrollView;//滑动布局
-    LinearLayout moveLayout;//滑动布局
     int safeTopDistance;//距离顶部安全距离(必须是刘海屏才有)
     int downY;//向下滑动时标题栏渐变点
     int upY;//向上滑动时标题栏渐变点
@@ -42,38 +29,103 @@ public class FriendCircleActivity extends AppCompatActivity implements View.OnTo
     int upScrollPart = 1;//上滑阶段 1,2
     int secondUpScrollPartPoint;//上滑第二阶段起点
     boolean topBarBackgroundTransparentMode = true; //当前topBar是否为背景透明模式
-    boolean allowUp = false;
-    int scrollY;
-    int tempDistance;
-
+    boolean allowUp = false;//是否允许处理 scrollView向上滚动时悬浮标题栏的渐变效果，为false时，只处理向下滚动时的效果，为true时，只处理向上滚动时的效果
+    int scrollY;//scrollView向下滚动距离
+    int tempDistance;//移动布局离原位置Y轴距离
+    boolean everTranslateY = false;//用户是否曾经向下拖动过布局
+    private float touchY;//用户手指按下点的Y轴坐标
+    FriendCircleBinding binding;//视图绑定对象
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //设置透明状态栏
         Utils.transparencyBar(this);
-        setContentView(R.layout.friend_circle);
+        //获取布局加载器
+        LayoutInflater layoutInflater = LayoutInflater.from(this);
+        //获取ViewBinding对象，免去繁琐的findViewById
+        binding = FriendCircleBinding.inflate(layoutInflater);
+        //binding.getRoot()返回绑定布局的根视图
+        setContentView(binding.getRoot());
         //获取刘海高度
         getSafeTopDistance();
-        initViews();
+        init();
         initData();
         initEvents();
-        Log.d(TAG, "onTouch: getTranslationY="+moveLayout.getTranslationY());
     }
+
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                //记住按下时手指Y轴坐标
+                touchY = event.getRawY();
+            }
+            break;
+            case MotionEvent.ACTION_MOVE: {
+                float moveY = (event.getRawY() - touchY);
+                Log.d(TAG, "onTouch: setTranslationY" + binding.friendCircleMoveLayout.getTranslationY());
+                //当滑动距离为0.即没有滑动的时候才能向下移动布局
+                if (scrollY <= 0) {
+                    binding.friendCircleMoveLayout.setTranslationY(moveY * 2 / 5 + tempDistance);
+                }
+                //如果TranslationY>0,证明此次向下拖动过布局，不是一开始就向下滚动
+                if (binding.friendCircleMoveLayout.getTranslationY() > 0) {
+                    everTranslateY = true;
+                }
+                if (binding.friendCircleMoveLayout.getTranslationY() > 0) {
+                    //TranslationY>0，即布局是向下拖动状态，return true阻止scrollView向下滚动
+                    return true;
+                } else {
+                    //用户曾经向下拖动过布局（即everTranslateY=true），再手动将布局拖回原位后不允许接着滚动scrollView，TranslationY=0时即布局拖回原位，TranslationY<0时即拖回原位后继续往上拖
+                    if (binding.friendCircleMoveLayout.getTranslationY() <= 0 && everTranslateY) {
+                        //不允许继续往上拖，保持布局在原位置·
+                        binding.friendCircleMoveLayout.setTranslationY(0);
+                        return true;
+                    }else {
+                        //everTranslateY=false 即用户没有向下拖动过布局，正常向下滚动scrollView
+                        return false;
+                    }
+                }
+            }
+            case MotionEvent.ACTION_UP: {
+                //重置用户是否向下拖动过布局的标记
+                everTranslateY = false;
+                //用户手指离开屏幕，复位布局
+                binding.friendCircleMoveLayout.setTranslationY(0);
+                //用户手指离开屏幕，复位下拉距离
+                tempDistance = 0;
+            }
+            break;
+            case MotionEvent.ACTION_POINTER_DOWN: {
+                //当第一个手指按下后，有其他手指按下，则记住当前移动布局离原位置距离，以便累加新手指的拖动距离
+                tempDistance = (int) binding.friendCircleMoveLayout.getTranslationY();
+                //当第一个手指按下后，有其他手指按下，则更新用户按下点Y轴坐标
+                touchY = event.getRawY();
+            }
+            break;
+        }
+        return false;
+    }
+
+
 
     private void initEvents() {
         FriendCircleScrollViewListener scrollViewListener = new FriendCircleScrollViewListener();
         scrollViewListener.setScrollYListener((scrollY, down) -> {
             this.scrollY = scrollY;
             if (!allowUp) {
+                //处理下滑悬浮标题效果
                 handleScrollDown(scrollY);
             } else {
+                //处理赏花悬浮标题效果
                 handleScrollUp(scrollY);
             }
         });
         //设置scrollView滑动监听
-        scrollView.setOnScrollChangeListener(scrollViewListener);
+        binding.friendCircleScrollView.setOnScrollChangeListener(scrollViewListener);
         //设置scrollView手势监听
-        scrollView.setOnTouchListener(this);
+        binding.friendCircleScrollView.setOnTouchListener(this);
     }
 
     private void handleScrollDown(int scrollY) {
@@ -102,7 +154,7 @@ public class FriendCircleActivity extends AppCompatActivity implements View.OnTo
                     downScrollPart = 1;
                 }
             }
-            topBar.setAlpha((float) alpha);
+            binding.friendCircleTopBar.setAlpha((float) alpha);
         }
     }
 
@@ -140,61 +192,52 @@ public class FriendCircleActivity extends AppCompatActivity implements View.OnTo
                     upScrollPart = 1;
                 }
             }
-            topBar.setAlpha((float) alpha);
+            binding.friendCircleTopBar.setAlpha((float) alpha);
         }
     }
 
     private void setTopBarBackgroundTransparentMode() {
-        backIcon.setImageResource(R.drawable.b6i);
-        backButton.setBackground(getDrawable(R.drawable.back_ripple_bg));
-        photoLayout.setBackground(getDrawable(R.drawable.photo_selector));
-        photoIcon.setReverse(true);
-        photoIcon.setReverseIconColor(getResources().getColor(R.color.white));
-        title.setVisibility(View.GONE);
-        topBar.setBackgroundColor(getResources().getColor(R.color.transparent));
+        binding.friendCircleBackIcon.setImageResource(R.drawable.b6i);
+        binding.friendCircleBack.setBackground(getDrawable(R.drawable.back_ripple_bg));
+        binding.friendCirclePhotoLayout.setBackground(getDrawable(R.drawable.photo_selector));
+        binding.friendCirclePhotoIcon.setReverse(true);
+        binding.friendCirclePhotoIcon.setReverseIconColor(getResources().getColor(R.color.white));
+        binding.friendCircleBackTitle.setVisibility(View.GONE);
+        binding.friendCircleTopBar.setBackgroundColor(getResources().getColor(R.color.transparent));
         Utils.transparencyBar(FriendCircleActivity.this);
     }
 
     private void setTopBarBackgroundLightMode() {
-        backIcon.setImageResource(R.drawable.b6c);
-        backButton.setBackground(getDrawable(R.drawable.photo_light_mode_selector));
-        photoLayout.setBackground(getDrawable(R.drawable.photo_light_mode_selector));
-        photoIcon.setReverse(true);
-        photoIcon.setReverseIconColor(getResources().getColor(R.color.black));
-        title.setVisibility(View.VISIBLE);
-        title.setTextColor(getResources().getColor(R.color.black));
-        topBar.setBackgroundColor(getResources().getColor(R.color.statusBarColors));
+        binding.friendCircleBackIcon.setImageResource(R.drawable.b6c);
+        binding.friendCircleBack.setBackground(getDrawable(R.drawable.photo_light_mode_selector));
+        binding.friendCirclePhotoLayout.setBackground(getDrawable(R.drawable.photo_light_mode_selector));
+        binding.friendCirclePhotoIcon.setReverse(true);
+        binding.friendCirclePhotoIcon.setReverseIconColor(getResources().getColor(R.color.black));
+        binding.friendCircleBackTitle.setVisibility(View.VISIBLE);
+        binding.friendCircleBackTitle.setTextColor(getResources().getColor(R.color.black));
+        binding.friendCircleTopBar.setBackgroundColor(getResources().getColor(R.color.statusBarColors));
         Utils.StatusBarLightMode(FriendCircleActivity.this);
     }
 
-    private void initViews() {
-        userHead = findViewById(R.id.friend_circle_user_head);
-        topBar = findViewById(R.id.friend_circle_top_bar);
-        backButton = findViewById(R.id.friend_circle_back);
-        backIcon = findViewById(R.id.friend_circle_back_icon);
-        title = findViewById(R.id.friend_circle_back_title);
-        photoIcon = findViewById(R.id.friend_circle_photo_icon);
-        photoLayout = findViewById(R.id.friend_circle_photo_layout);
-        scrollView = findViewById(R.id.friend_circle_scroll_view);
-        moveLayout = findViewById(R.id.friend_circle_move_layout);
-        title.setVisibility(View.GONE);
+    private void init() {
+        binding.friendCircleBackTitle.setVisibility(View.GONE);
         //根据屏幕刘海高度动态设置标题栏padding_top属性，以防刘海遮挡内容
-        topBar.post(new Runnable() {
+        binding.friendCircleTopBar.post(new Runnable() {
             @Override
             public void run() {
-                topBar.setPadding(0, safeTopDistance, 0, 0);
+                binding.friendCircleTopBar.setPadding(0, safeTopDistance, 0, 0);
             }
         });
     }
 
     private void initData() {
-        downY = Utils.dpTpPx(190, FriendCircleActivity.this);
+        downY = Utils.dpTpPx(180, FriendCircleActivity.this);
         upY = Utils.dpTpPx(230, FriendCircleActivity.this);
         Glide.with(this)
                 .load("https://img-blog.csdnimg.cn/20190918140145169.png")
                 .centerCrop()
                 .apply(RequestOptions.bitmapTransform(new RoundedCorners(Utils.dpTpPx(6, FriendCircleActivity.this))))//圆角半径
-                .into(userHead);
+                .into(binding.friendCircleUserHead);
     }
 
     @TargetApi(28)
@@ -204,50 +247,17 @@ public class FriendCircleActivity extends AppCompatActivity implements View.OnTo
             @Override
             public void run() {
                 DisplayCutout displayCutout = decorView.getRootWindowInsets().getDisplayCutout();
-                safeTopDistance = displayCutout.getSafeInsetTop();
+                if (displayCutout != null) {
+                    safeTopDistance = displayCutout.getSafeInsetTop();
+                }
             }
         });
     }
 
-    private float touchY;
-    private float lastMoveY;
 
     @Override
-    public boolean onTouch(View v, MotionEvent event) {
-            switch (event.getAction()) {
-                case MotionEvent.ACTION_DOWN: {
-                    touchY = event.getRawY();
-                    lastMoveY= touchY;
-                }
-                break;
-                case MotionEvent.ACTION_MOVE: {
-
-                    float moveY = (event.getRawY() - touchY);
-                    if (lastMoveY > event.getRawY()) {//手指上滑
-                        Log.d(TAG, "onTouch: 手指上滑");
-                        moveLayout.setTranslationY(moveY * 2/5 + tempDistance);
-                        Log.d(TAG, "onTouch: setTranslationY"+moveLayout.getTranslationY());
-                        if (moveLayout.getTranslationY() <= 340) {
-                            return true;
-                        }
-                    } else {//手指下滑
-                        Log.d(TAG, "onTouch: 手指下滑");
-                        moveLayout.setTranslationY(moveY * 2 / 5 + tempDistance);
-                    }
-                    lastMoveY = event.getRawY();
-                }
-                break;
-                case MotionEvent.ACTION_UP: {
-                    moveLayout.setTranslationY(0);
-                    tempDistance = 0;
-                }
-                break;
-                case MotionEvent.ACTION_POINTER_DOWN: {
-                    tempDistance = (int) moveLayout.getTranslationY();
-                    touchY = event.getRawY();
-                }
-                break;
-        }
-        return false;
+    public boolean onTouchEvent(MotionEvent event) {
+        Log.d(TAG, "onTouchEvent: ");
+        return super.onTouchEvent(event);
     }
 }
